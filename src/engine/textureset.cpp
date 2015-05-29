5b4f26e1d3766d090d839bb2f785fa77ee6ff89c
a_teammate <madoe3@web.de>
2015-05-30 01:29:11 +0200

    Move class textureset to own header file

--- src/engine/textureset.cpp
+++ src/engine/textureset.cpp
@@ -4,20 +4,9 @@
 ///  Date:   15.03.2015
 ///
 
-///// WIP FLAG WIP FLAG
-///
-/// Inexors routine load and process ingame-textures depends on 3 structures:
-/// VSlot       - A Structure holding general info about scale, shadersettings, rotation, ..
-/// Slot        - Basicly thats a "Texture" as you would find it ingame in the texturebrowser.
-///               It consists of minimum 1 image (the diffuse-file) and various others providing special info (height,..).
-///               Every Slot has a VSlot.
-/// textureset  - A Set of textures/Slots to make loading easier.
-///
-/// The currently visible stack of ingame-textures (Slots) is vector slots.
-///// Just used to sync atm, with all stuff thrown in just so its here
-
 #include "engine.h"
 #include "texture.h"
+#include "textureset.h"
 
 namespace inexor {
     namespace textureset {
@@ -43,73 +32,6 @@ namespace inexor {
         }
 
         const char *jsontextures[8] = { "diffuse", "other", "decal", "normal", "glow", "spec", "depth", "envmap" };
-
-        /// 
-        class textureset
-        {
-        private:
-
-            /// One entry to the set
-            class texentry
-            {
-            public:
-                /// A bitmask containing which of the 8 textures of the slot already have been loaded.
-                int loadmask = 0;
-
-                /// Whether this Slot is usable ingame.
-                bool mounted = false;
-
-                /// Whether or not this Slot is actually just a pointer to another occurence
-                //bool reference = false;
-
-                /// The Texture-Slot
-                Slot *tex;
-
-                texentry(Slot *s)
-                {
-                    tex = s;
-                }
-            };
-
-        public:
-            /// All included Slots.
-            vector<texentry *>texs;
-
-            /// Adds a texture to the set from a JSON (Object).
-            void addtexture(JSON *j);
-
-            /// Adds a texture to the set from a json-file.
-            void addtexture(const char *filename);
-
-            /// Checks if textures may do not need to be loaded since they are already stored somewhere
-            void checkload();
-
-            /// Loads all textures to memory.
-            /// This function is threadsafe.
-            /// @usage 1. checkload (not threaded) 2. load (threaded) 3. registerload (not threaded)
-            void load();
-
-            /// Saves loaded textures to the texture registry.
-            void registerload();
-
-            /// Add this textureset to the current texture stack of ingame visible textures.
-            /// @param initial if true this textureset becomes the first only one.
-            void mount(bool initial);
-
-            /// Mounts remaining textures
-            void mountremaining();
-
-            /// Removes this textureset from the current stack of ingame visible textures.
-            /// Attention: all following slots will be change its position and hence this has an visual impact ingame! TODO!!
-            void unmount();
-
-            void echoall()
-            {
-                loopv(texs) {
-                    if(texs[i]->tex->sts.length())  conoutf("tex %d: %s", i, texs[i]->tex->sts.last().name);
-                }
-            }
-        };
 
         void textureset::addtexture(JSON *j)
         {
@@ -169,7 +91,8 @@ namespace inexor {
                 texentry *t = texs[i];
                 int diff = t->tex->texmask & ~t->loadmask; // All textures which havent been loaded yet
                 if(diff) loopi(8) //TODO REPLACE WITH TEX_NUM
-                { //TODO BREAK IF TOO MUCH OUT OF STS
+                {
+                    if(i >= t->tex->sts.length()) break; // out of range
                     if(!(diff & (1 << i))) continue; // not in diff
                     Slot::Tex &curimg = t->tex->sts[i];
                     curimg.t = gettexture(curimg.name);
---
+++ src/engine/textureset.h
@@ -0,0 +1,95 @@
+///  Texturesets are used to load texture-stacks asynchronously
+///
+///  Author: Malte "a_teammate" Haase
+///  Date:   15.03.2015
+///
+
+///// GENERAL:
+///
+/// Inexors routine of loading and storing ingame-textures depends on 3 structures:
+/// VSlot       - A Structure holding general info about scale, shadersettings, rotation, ..
+/// Slot        - Basicly thats a "Texture" as you would find it ingame in the texturebrowser.
+///               It consists of minimum 1 image (the diffuse-file) and various others providing special info (height,..).
+///               Every Slot has a VSlot. (and vice versa: every VSlot has at least one Slot)
+/// textureset  - A Set of textures/Slots to make loading easier and asynchronous.
+///
+/// The currently visible stack of ingame-textures (Slots) is vector<Slot *> slots.
+
+#ifndef TEXTURESET_H
+#define TEXTURESET_H
+
+namespace inexor {
+    namespace textureset {
+        /// A set of Slots to make threaded loading possible. 
+        class textureset
+        {
+        private:
+
+            /// One entry to the set
+            class texentry
+            {
+            public:
+                /// A bitmask containing which of the 8 textures of the slot already have been loaded.
+                int loadmask = 0;
+
+                /// Whether this Slot is usable ingame.
+                bool mounted = false;
+
+                /// Whether or not this Slot is actually just a pointer to another occurence
+                //bool reference = false;
+
+                /// The Texture-Slot
+                Slot *tex;
+
+                texentry(Slot *s)
+                {
+                    tex = s;
+                }
+            };
+
+        public:
+            /// All included Slots.
+            vector<texentry *>texs;
+
+            /// Adds a texture to the set from a JSON (Object).
+            void addtexture(JSON *j);
+
+            /// Adds a texture to the set from a json-file.
+            void addtexture(const char *filename);
+
+            /// Checks if textures may do not need to be loaded since they are already stored somewhere
+            void checkload();
+
+            /// Loads all textures to memory.
+            /// This function is threadsafe.
+            /// @usage 1. checkload (not threaded) 2. load (threaded) 3. registerload (not threaded)
+            void load();
+
+            /// Saves loaded textures to the texture registry.
+            void registerload();
+
+            /// Add this textureset to the current texture stack of ingame visible textures.
+            /// @param initial if true this textureset becomes the first only one.
+            void mount(bool initial);
+
+            /// Mounts remaining textures
+            void mountremaining();
+
+            /// Removes this textureset from the current stack of ingame visible textures.
+            /// Attention: all following slots will be change its position and hence this has an visual impact ingame! TODO!!
+            void unmount();
+
+            void echoall()
+            {
+                loopv(texs) {
+                    if(texs[i]->tex->sts.length())  conoutf("tex %d: %s", i, texs[i]->tex->sts.last().name);
+                }
+            }
+        };
+
+        extern textureset *newtextureset(JSON *parent);
+
+    } // namespace textureset
+}     // namespace inexor
+
+#endif //TEXTURESET_H
