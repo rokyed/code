ba4f11309ef4aa8fc75c329bbc02958fa360c573
a_teammate <madoe3@web.de>
2015-10-27 19:46:08 +0100

    texture: remove all remaining slots globals usage in legacy loading

--- inexor/texture/legacyslotload.cpp
+++ inexor/texture/legacyslotload.cpp
@@ -4,6 +4,8 @@
 #include "inexor/shared/filesystem.h"
 #include "inexor/texture/slotregistry.h"
 
+using namespace inexor::texture;
+
 const struct slottex
 {
     const char *name;
@@ -31,6 +33,7 @@ int findslottex(const char *name)
 
 void texture(char *type, char *name, int *rot, int *xoffset, int *yoffset, float *scale)
 {
+    vector<Slot *> &slots = getcurslotreg()->slots;
     if(slots.length() >= 0x10000) return;
     if(!type || !name) return;
 
@@ -39,7 +42,7 @@ void texture(char *type, char *name, int *rot, int *xoffset, int *yoffset, float
     if(tnum < 0) tnum = atoi(type);
     if(tnum == TEX_DIFFUSE) lastmatslot = matslot;
     else if(lastmatslot >= 0) matslot = lastmatslot;
-    else if(slots.empty()) return;
+    else if(!numcurslots()) return;
     Slot &s = matslot >= 0 ? lookupmaterialslot(matslot, false) : *(tnum != TEX_DIFFUSE ? slots.last() : slots.add(new Slot(slots.length())));
     s.loaded = 0;
     s.texmask |= 1 << tnum;
@@ -65,7 +68,8 @@ COMMAND(texture, "ssiiif");
 
 void autograss(char *name)
 {
-    if(slots.empty()) return;
+    vector<Slot *> &slots = getcurslotreg()->slots;
+    if(!numcurslots()) return;
     Slot &s = *slots.last();
     DELETEA(s.autograss);
     s.autograss = name[0] ? newstring(makerelpath(getcurexecdir(), name, NULL, "<premul>")) : NULL;
@@ -75,28 +79,28 @@ COMMAND(autograss, "s");
 /// Set the texture scrolling of the specified Slot to the given amount of x and y.
 void slotscroll(int slotid, float scrollS, float scrollT)
 {
-    if(slots.empty() || !slots.inrange(slotid)) return;
-    slots[slotid]->setscroll(scrollS, scrollT);
+    if(hasslot(slotid)) return;
+    lookupslot(slotid, false).setscroll(scrollS, scrollT);
 }
 
 void texscroll(float *scrollS, float *scrollT)
 {
-    if(slots.empty()) return;
-    slotscroll(slots.last()->index, *scrollS, *scrollT);
+    if(!numcurslots()) return;
+    slotscroll(getcurslotreg()->slots.last()->index, *scrollS, *scrollT);
 }
 COMMAND(texscroll, "ff");
 
 /// Shift the texture by a given percentage to x or y for given Slot.
 void slotoffset(int slotid, int xoffset, int yoffset)
 {
-    if(slots.empty() || !slots.inrange(slotid)) return;
-    slots[slotid]->setoffset(xoffset, yoffset);
+    if(hasslot(slotid)) return;
+    lookupslot(slotid, false).setoffset(xoffset, yoffset);
 }
 
 void texoffset_(int *xoffset, int *yoffset)
 {
-    if(slots.empty()) return;
-    slotoffset(slots.last()->index, *xoffset, *yoffset);
+    if(!numcurslots()) return;
+    slotoffset(getcurslotreg()->slots.last()->index, *xoffset, *yoffset);
 
 }
 COMMANDN(texoffset, texoffset_, "ii");
@@ -105,36 +109,36 @@ COMMANDN(texoffset, texoffset_, "ii");
 /// @param rot param for the rotation between 0 and 3 (0 or 4 beeing standard)
 void slotrotate(int slotid, int rot)
 {
-    if(slots.empty() || !slots.inrange(slotid)) return;
-    slots[slotid]->setrotate(rot);
+    if(hasslot(slotid)) return;
+    lookupslot(slotid, false).setrotate(rot);
 }
 
 void texrotate_(int *rot)
 {
-    if(slots.empty()) return;
-    slotrotate(slots.last()->index, *rot);
+    if(!numcurslots()) return;
+    slotrotate(getcurslotreg()->slots.last()->index, *rot);
 }
 COMMANDN(texrotate, texrotate_, "i");
 
 /// Scale the texture by the given amount for the given Slot, will be clamped to poweroftwo values.
 void slotscale(int slotid, float scale)
 {
-    if(slots.empty() || !slots.inrange(slotid)) return;
-    slots[slotid]->setscale(scale);
+    if(hasslot(slotid)) return;
+    lookupslot(slotid, false).setscale(scale);
 }
 
 void texscale(float *scale)
 {
-    if(slots.empty()) return;
-    slotscale(slots.last()->index, *scale);
+    if(!numcurslots()) return;
+    slotscale(getcurslotreg()->slots.last()->index, *scale);
 }
 COMMAND(texscale, "f");
 
-void texlayer(int *layer, char *name, int *mode, float *scale)
+void texlayer(int *layer, char *name, int *mode, float *scale) // TODO
 {
-    if(slots.empty()) return;
-    Slot &s = *slots.last();
-    s.variants->layer = *layer < 0 ? max(slots.length() - 1 + *layer, 0) : *layer;
+    if(!numcurslots()) return;
+    Slot &s = *getcurslotreg()->slots.last();
+    s.variants->layer = *layer < 0 ? max(numcurslots() - 1 + *layer, 0) : *layer;
     s.layermaskname = name[0] ? newstring(path(makerelpath(getcurexecdir(), name))) : NULL;
     s.layermaskmode = *mode;
     s.layermaskscale = *scale <= 0 ? 1 : *scale;
@@ -145,14 +149,14 @@ COMMAND(texlayer, "isif");
 /// Set the transparency of given slot to given amount.
 void slotalpha(int slotid, float front, float back)
 {
-    if(slots.empty() || !slots.inrange(slotid)) return;
-    slots[slotid]->setalpha(front, back);
+    if(hasslot(slotid)) return;
+    lookupslot(slotid, false).setalpha(front, back);
 }
 
 void texalpha(float *front, float *back)
 {
-    if(slots.empty()) return;
-    slotalpha(slots.last()->index, *front, *back);
+    if(!numcurslots()) return;
+    slotalpha(getcurslotreg()->slots.last()->index, *front, *back);
 }
 COMMAND(texalpha, "ff");
 
@@ -160,14 +164,14 @@ COMMAND(texalpha, "ff");
 /// param r floatvalue between 0 and 1, giving the red amount.
 void slotcolor(int slotid, float r, float g, float b)
 {
-    if(slots.empty() || !slots.inrange(slotid)) return;
-    slots[slotid]->setcolor(r, g, b);
+    if(hasslot(slotid)) return;
+    lookupslot(slotid, false).setcolor(r, g, b);
 }
 
 void texcolor(float *r, float *g, float *b)
 {
-    if(slots.empty()) return;
-    slotcolor(slots.last()->index, *r, *g, *b);
+    if(!numcurslots()) return;
+    slotcolor(getcurslotreg()->slots.last()->index, *r, *g, *b);
 }
 COMMAND(texcolor, "fff");
 
